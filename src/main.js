cdf17a7cf0f91c30ecb0a9102471afd9d9fed41c
Salsicha <am.96138669@gmail.com>
2025-09-09 10:54:00 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,37 +1,31 @@
 import './style.css'
 import Phaser, { CANVAS, Physics, Scene } from 'phaser'
 
+
+
 const tamanho = {
   width:1200,
-  height:700,
-}
+  height:700,}
 
 const hitboxPorco = {
   width: 100,
-  height: 75,
-}
+  height: 75,}
 
 const hitboxPod = {
   width:50,
-  height:90,
-}
+  height:90,}
 
 const hitboxCecco = {
   width:200,
-  height:180,
-}
+  height:180,}
 
-
-//gravidade
 const speedDown = 600
-
 
 const inicio = document.querySelector("#inicio")
 const inicioBTN = document.querySelector("#inicioBTN")
 const fim = document.querySelector("#fim")
 const ganhaPerde = document.querySelector("ganhaPerde")
 const reload = document.querySelector("reload")
-
 
 class gameScene extends Phaser.Scene{
   constructor(){
@@ -44,11 +38,8 @@ class gameScene extends Phaser.Scene{
     this.textPods
     this.textParceria
     this.podQueimado
-    this.magrao
-  }
-
-
-
+    this.magrao}
+  
   preload(){
     this.load.image("fundo","/assets/Guarita.jpg")
     this.load.image("player","/assets/bustoDoCecco.png");
@@ -56,85 +47,46 @@ class gameScene extends Phaser.Scene{
     this.load.image("podQueimado","/assets/porco3.png");
   }
 
-
-
-
-
   create(){
-
-    this.scene.pause("scene-game")
-
-//fundo
+  this.scene.pause("scene-game")
   this.add.image(0,0,"fundo").setOrigin(0,0);
-
-//player
   this.player = this.physics.add.image(0,tamanho.height-100,"player").setOrigin(0,0);
   this.player.setImmovable(true);
   this.player.body.allowGravity = false
   this.player.setCollideWorldBounds(true);
   this.player.setSize(this.player.width - this.player.width / 4, this.player.height / 6).setOffset(
   this.player.width / 10, this.player.height - this.player.height / 10);
-
-//pod 
   this.target = this.physics.add.image(this.getRandomX(), 100, "pod").setOrigin(0, 0);
   this.target.setSize(hitboxPod.width, hitboxPod.height);
   this.target.setOffset(0, 0);
   this.target.setMaxVelocity(speedDown);
   this.physics.add.overlap(this.target, this.player,this.targetHit, null, this)
-
-
-
-//pod queimado
   this.podQueimado = this.physics.add.image(this.getRandomX(), 100, "podQueimado").setOrigin(0, 0);
   this.podQueimado.setSize(hitboxPorco.width, hitboxPorco.height);
   this.podQueimado.setOffset(0, 0);
   this.podQueimado.setMaxVelocity(speedDown);
   this.physics.add.overlap(this.podQueimado, this.player, this.perder, null, this)
   this.podQueimado.setScale(2);
-
-
-
-  //texto magrão
   this.magrao=this.add.text(100,100, "teste",{
     font: "25px Arial",
-    fill: "#000000",
-  })
-
-
+    fill: "#000000",})
   this.cursor = this.input.keyboard.createCursorKeys();
-
-
   this.textParceria = this.add.text(tamanho.width - 1000, 10, "Ajude o Cecconelo a Pegar 30 Pods!!",{
     font: "25px Arial",
-    fill: "#000000",
-  })
-
+    fill: "#000000",})
   this.textPods = this.add.text(tamanho.width - 120, 10, "Pontos:0" , {
     font: "25px Arial",
-    fill: "#000000",
-  });
-
-  }
-
-
-
-
+    fill: "#000000",});}
+  
   update(){
 
 if (this.target.y >= tamanho.height){
   this.target.setY(0);
-  this.target.setX(this.getRandomX())
-}
-
+  this.target.setX(this.getRandomX())}
 if (this.podQueimado.y >= tamanho.height){
   this.podQueimado.setY(0);
-  this.podQueimado.setX(this.getRandomX())
-}
-  
-
-
+  this.podQueimado.setX(this.getRandomX())}
     const {left,right} = this.cursor
-
     if (left.isDown){
       this.player.setVelocityX(-this.playerSpeed); 
     }else if (right.isDown){
@@ -142,21 +94,16 @@ if (this.podQueimado.y >= tamanho.height){
     }else {
       this.player.setVelocityX(0)
     }
-
-
     if (this.points > 29){
       this.textParceria.setText(`isso aí magrão, parceria`)
       this.points = 0;
     };
-
   }
 
+  
 getRandomX(){
   return Math.floor(Math.random()* tamanho.width);
 };
-
-
-
 
 targetHit (){
   this.target.setY(0);
@@ -165,21 +112,13 @@ targetHit (){
   this.textPods.setText(`Pods: ${this.points}`)
 }
 
-
 perder (){
   this.sys.game.destroy(true)
   this.podQueimado.setY(0);
   this.podQueimado.setX(this.getRandomX());
   this.points=0;
   this.textParceria.setText(`perdeu bixo`)
-  fim.style.display="flex"
-
-}
-
-
-
-}
-
+  fim.style.display="flex"}}
 
 const config = {
   type: Phaser.WEBGL,
@@ -193,7 +132,6 @@ const config = {
   },
   scene:[gameScene]
 }
-
 
 const game = new Phaser.Game(config)
 
